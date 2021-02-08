fec5d877329cdf7dc2cfca8d6b8fa01d5151c91a
James Hooper <jameshooperdesigns@gmail.com>
2021-02-08 16:44:35 -0700

    Add two functions to the cf.js

--- custom-functions/cf.js
+++ custom-functions/cf.js
@@ -1,10 +1,30 @@
 // Function Definition
-function calculateBill() {
+
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
   // This is the function body
-  const total = 100 * 1.13;
+  const total = billAmount + billAmount * taxRate + billAmount * tipRate;
   return total;
 }
 
 // Function Call or **Run**
-const myTotalBill = calculateBill();
-console.log(`Your total bill is: $${myTotalBill}`);
+const jamesTotal = 500;
+const jamesTaxRate = 0.5;
+const myTotal = calculateBill(jamesTotal, jamesTaxRate);
+
+function sayHiTo(firstName) {
+  return `Hello ${firstName}`;
+}
+
+// const greeting = sayHiTo('James');
+// console.log(greeting);
+
+function doctorize(name) {
+  return `Dr ${name}`;
+}
+
+function yell(name = 'Silly Goose') {
+  return `HEY ${name.toUpperCase()}`;
+}
+
+const myBill4 = calculateBill(100, undefined, 0.2);
+console.log(myBill4);
