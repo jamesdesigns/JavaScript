dc16ee78528391c4b17e027c63d378692a7261ca
James Hooper <jameshooperdesigns@gmail.com>
2021-02-10 15:10:33 -0700

    Add a click callback function to file

--- custom-functions/ways-to-make-a-function.js
+++ custom-functions/ways-to-make-a-function.js
@@ -32,4 +32,33 @@
 // }
 
 // Returning an Object (NEW WAY)
-const makeABaby = (first, last) => ({ name: `${first} ${last}`, age: 0 });
+// const makeABaby = (first, last) => ({ name: `${first} ${last}`, age: 0 });
+
+// IIFE
+// Immediately Invoked Function Expression
+// (function(age) {
+//   return `You are cool and age ${age}`;
+// })(10);
+
+// Methods!!
+const james = {
+  name: 'James Hooper',
+  // Short Hand Method - MOST COMMON WAY
+  sayHi() {console.log('Hi!')},
+  // Arrow Function Method
+  whisperHi: () => {
+    console.log('Heyyy James');
+  }
+}
+
+// Call Back Functions
+// Click Callback
+const button = document.querySelector('.clickMe');
+function handleClick() {
+  console.log('Great clicking!');
+}
+
+button.addEventListener('click', handleClick); // james.sayHi IS the callback function
+
+// Timer Callback
+setTimeout(james.sayHi, 1000); // Run this timer callback after 1 second
