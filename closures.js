66cc10e72fefd12b910efab9d82773b1cbef1c09
James Hooper <jameshooperdesigns@gmail.com>
2021-02-16 23:40:51 -0700

    Add closures.html and .js file

---
+++ closures.js
@@ -0,0 +1,8 @@
+function outer() {
+  const outerVar = 'Hey I am the outer var';
+  function inner() {
+    const innerVar = 'Hey I am an inner var';
+    console.log(innerVar);
+    console.log(outerVar);
+  }
+}
