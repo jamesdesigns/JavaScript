05e6f3b7be6e43d2807ce7f8536a58751bb123f6
James Hooper <jameshooperdesigns@gmail.com>
2021-02-22 12:13:53 -0700

    Use the insertAdjacentText to add an element before and after the p tag

--- the-dom.js
+++ the-dom.js
@@ -2,6 +2,13 @@ const p = document.querySelector('p');
 const imgs = document.querySelectorAll('.item img');
 const item2 = document.querySelector('.item2');
 const item2Image = item2.querySelector('img');
-const heading = document.querySelector('h2');
+// const heading = document.querySelector('h2');
+// heading.textContent = 'James is cool.';
 
-console.log(heading);
+const hotdogList = document.querySelector('.hotDog');
+
+console.log(hotdogList.textContent);
+
+// hotdogList.textContent = `${hotdogList.textContent} 🌭`;
+hotdogList.insertAdjacentText('afterbegin', '🌭');
+hotdogList.insertAdjacentText('beforeend', '🌭');
