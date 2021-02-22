37c2f4a1343586c6d82bc5081d53e5737f7ebb41
James Hooper <jameshooperdesigns@gmail.com>
2021-02-22 12:36:02 -0700

    Add a toggle with an addEventListener

--- the-dom.js
+++ the-dom.js
@@ -1,14 +1,27 @@
-const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.item img');
-const item2 = document.querySelector('.item2');
-const item2Image = item2.querySelector('img');
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
+// const item2 = document.querySelector('.item2');
+// const item2Image = item2.querySelector('img');
 // const heading = document.querySelector('h2');
 // heading.textContent = 'James is cool.';
 
-const hotdogList = document.querySelector('.hotDog');
-
-console.log(hotdogList.textContent);
+// const hotdogList = document.querySelector('.hotDog');
+// console.log(hotdogList.textContent);
 
 // hotdogList.textContent = `${hotdogList.textContent} 🌭`;
-hotdogList.insertAdjacentText('afterbegin', '🌭');
-hotdogList.insertAdjacentText('beforeend', '🌭');
+// hotdogList.insertAdjacentText('afterbegin', '🌭');
+// hotdogList.insertAdjacentText('beforeend', '🌭');
+
+// Classes
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.remove('cool');
+pic.classList.toggle('round');
+
+console.log(pic.classList);
+
+function toggleRound() {
+  pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
